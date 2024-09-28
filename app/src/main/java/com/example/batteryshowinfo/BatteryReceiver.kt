563dca6ebad915dd6f2001a2ced58f00d16db68f
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2024-09-27 22:56:07 -0500

    creacion battery receiver

---
+++ app/src/main/java/com/example/batteryshowinfo/BatteryReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.batteryshowinfo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BatteryReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // TODO("BatteryReceiver.onReceive() is not implemented")
+    }
+}
