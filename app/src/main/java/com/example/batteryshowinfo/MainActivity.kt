fdc37d6e7f38950d066e17c65d23789fe6ac61de
Gabriel <gmamanican@unsa.edu.pe>
2024-10-02 10:21:15 -0500

    add connection between Broadcast and Activity with listener

--- app/src/main/java/com/example/batteryshowinfo/MainActivity.kt
+++ app/src/main/java/com/example/batteryshowinfo/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.batteryshowinfo
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import android.view.animation.AnimationUtils
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.batteryshowinfo.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BatteryListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var batteryReceiver: BatteryReceiver
+    private val STATE_APP = "StateAppMainActivity"
+
+    // Variables interfaz
+    private var currentStateBattery = binding.currentStateBattery
+    private var energySourceBattery = binding.energySourceBattery
+    private var percentageBattery = binding.percentageBattery
+    private var batteryImage = binding.batteryImage
+    private var generalStateBattery = binding.generalStateBattery
+    private var temperatureBattery = binding.temperatureBattery
+    // batteryImage.setImageResource(R.drawable.baseline_battery_0_bar_24)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +40,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        Log.d(STATE_APP, "Start")
         // Registrando receiver
 
-        batteryReceiver = BatteryReceiver()
+        batteryReceiver = BatteryReceiver(this)
 
         // Registrar el receiver para obtener la información de la batería
         // Intent proporcionado por documentación de AndroidStudio
@@ -43,8 +52,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStop() {
+        super.onStop()
+        Log.d(STATE_APP, "Stop")
+    }
+
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(STATE_APP, "Destroy")
         unregisterReceiver(batteryReceiver)
+    }
+
+    override fun onBatteryInfoReceived(
+        isCharging: Boolean,
+        energySourceBattery: String,
+        batteryPct: Float,
+        batteryState: String,
+        batteryTemperature: Int
+    ) {
+
+        val batteryInfo = """
+            Cargando: $isCharging
+            Tipo de carga: $energySourceBattery
+            Porcentaje de batería: $batteryPct%
+            Estado batería: $batteryState
+            Temperatura: $batteryTemperature °C
+        """
+        // Log.d("MainActivityShowInfo", batteryInfo)
     }
 }
