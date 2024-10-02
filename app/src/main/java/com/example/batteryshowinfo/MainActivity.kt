bf7f50ee679c3fb1145c026149f4cc0bebf5c6a7
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-10-01 20:09:45 -0500

    register receiver in MainActivity

--- app/src/main/java/com/example/batteryshowinfo/MainActivity.kt
+++ app/src/main/java/com/example/batteryshowinfo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.batteryshowinfo
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +14,7 @@ import com.example.batteryshowinfo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var batteryReceiver: BatteryReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +27,24 @@ class MainActivity : AppCompatActivity() {
         mainLayout.startAnimation(animation)
 
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Registrando receiver
+
+        batteryReceiver = BatteryReceiver()
+
+        // Registrar el receiver para obtener la información de la batería
+        // Intent proporcionado por documentación de AndroidStudio
+        val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(batteryReceiver, filter)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(batteryReceiver)
     }
 }
