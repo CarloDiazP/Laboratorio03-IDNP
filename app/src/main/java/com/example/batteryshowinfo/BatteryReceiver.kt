eda4ac9652a1423e3718bf8f1d264bb4f84427ef
Gabriel Antony <gmamanican@unsa.edu.pe>
2024-10-01 19:59:51 -0500

    get battery info and show from receiver

--- app/src/main/java/com/example/batteryshowinfo/BatteryReceiver.kt
+++ app/src/main/java/com/example/batteryshowinfo/BatteryReceiver.kt
@@ -3,10 +3,64 @@ package com.example.batteryshowinfo
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.BatteryManager
+import android.util.Log
+import android.widget.Toast
 
 class BatteryReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // TODO("BatteryReceiver.onReceive() is not implemented")
+
+        // Obtener informacíon de la batería
+        val currentState: Int = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
+        val isCharging: Boolean = currentState == BatteryManager.BATTERY_STATUS_CHARGING || currentState == BatteryManager.BATTERY_STATUS_FULL
+
+        // Ver el tipo de fuente de carga
+        val chargePlug: Int = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) ?: -1
+        val usbCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
+        val acCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
+
+        // Selecciona el tipo de fuente de carga
+        val energySourceBattery = when {
+            // las variables actual como booleano, la que sea tru retornará el string señalado
+            usbCharge -> "USB"
+            acCharge -> "AC"
+            else -> "No charging"
+        }
+        // Se obtiene el % de la batería
+        val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+        val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        val batteryPct: Float = level * 100 / scale.toFloat()
+
+        // Obtener información del estado de la batería
+        val batteryHealth = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, BatteryManager.BATTERY_HEALTH_UNKNOWN)
+        val batteryState = when (batteryHealth) {
+            BatteryManager.BATTERY_HEALTH_GOOD -> "En buen estado"
+            BatteryManager.BATTERY_HEALTH_COLD -> "Fría"
+            BatteryManager.BATTERY_HEALTH_DEAD -> "Muerta"
+            BatteryManager.BATTERY_HEALTH_OVERHEAT -> "Sobrecalentada"
+            BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> "Sobre voltaje"
+            BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE -> "Fallo no especificado"
+            BatteryManager.BATTERY_HEALTH_UNKNOWN -> "Desconocido"
+            else -> "Desconocido"
+        }
+
+        // Obtener temperatura
+        val batteryTemperature: Int = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10
+
+
+        /////////////////////////////////////////////
+        val batteryInfo = """
+            Cargando: $isCharging
+            Tipo de carga: $energySourceBattery
+            Porcentaje de batería: $batteryPct%
+            Estado batería: $batteryState
+            Temperatura: $batteryTemperature °C
+        """
+
+        // Mostrar la información  de prueba desde Log
+        Log.d("BatteryInfo", batteryInfo)
+
+
     }
 }
